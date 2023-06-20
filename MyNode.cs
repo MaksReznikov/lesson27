d97bfbbb2c4ec30ab0fb5379982f61e30560e3a7
Maks <maks.reznikov.1999@gmail.com>
2023-06-20 20:31:16 +0600

    Initial

---
+++ MyNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson26
+{
+    internal class MyNode<T>
+    {
+        T Value;
+        MyNode<T> Next;
+        MyNode<T> Previous;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace lesson26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> list = new SortedList<int, string>();
+            list.Add(3, "qwe");
+            list.Add(1, "asd");
+            list.Add(2, "zxc");
+            foreach (var i in list.Keys)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            SortedDictionary<int, string> d = new SortedDictionary<int, string>();
+            d.Add(3, "qwe");
+            d.Add(1, "asd");
+            d.Add(2, "zxc");
+            foreach(var i in d.Keys)
+            {
+                Console.WriteLine(i);
+            }
+            LinkedListNode<int> n;
+            LinkedList<int> i = new LinkedList<int>();
+        }
+    }
+}
